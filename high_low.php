251fb49e5bb129d416400efbb15971cb271f5ac8
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-12 15:44:03 -0500

    Regular high low with 7 guesses

--- high_low.php
+++ high_low.php
@@ -1,28 +1,17 @@
 <?php
-//Constants
-define('MIN', $argv[1]);
-define ('MAX', $argv[2]);
 
-if (is_numeric(MIN) == FALSE && is_numeric(MAX) == FALSE) {
-	echo "You must use numbers!\n";
-	exit(0);	
-}
 //Computer needs to make random number
 
 //Have computer ask for number
 fwrite(STDOUT, 'Guess a number. ');
 
-$random = mt_rand(MIN, MAX);
+$random = mt_rand(1, 100);
 
 //Have a counter of how many guesses
 $number_of_guesses = 1;
 //Have user input number
 
 $guess = fgets(STDIN);
-
-
-
-
 
 
 //Tell user high, lower, or they were right
